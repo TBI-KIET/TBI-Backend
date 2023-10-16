8730cc8e9f3fb6d26f7e7128a74790d0fc358da8
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-10-17 01:46:38 +0530

    Event Detail API

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import EventsAPIView, ContactAPIView
+from .views import EventsAPIView, ContactAPIView, PastEventDetailView
 
 urlpatterns = [
     path('events/',EventsAPIView.as_view(), name = 'events'),
+    path('past-events/<slug:slug>/', PastEventDetailView.as_view(), name='past-event-detail'),
     path('contact/', ContactAPIView.as_view(), name = 'contact')
 ]
